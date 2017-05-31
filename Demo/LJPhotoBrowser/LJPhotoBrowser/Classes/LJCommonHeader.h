6feea2ad3731ce3ca5e0b851dad46d4da8646098
liuliangju <liangjuliu@163.com>
2017-05-31 15:27:00 +0800

    Change the inheritance of the controller

---
+++ Demo/LJPhotoBrowser/LJPhotoBrowser/Classes/LJCommonHeader.h
@@ -0,0 +1,13 @@
+//
+//  LJCommonHeader.h
+//  LJPhotoBrowser
+//
+//  Created by liangju on 5/31/17.
+//  Copyright © 2017 https://liuliangju.github.io. All rights reserved.
+//
+
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
