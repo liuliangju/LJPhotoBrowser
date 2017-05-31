d00df8a37e9f22af285144d0a3c9bc9787dbd390
liuliangju <liangjuliu@163.com>
2017-05-31 16:06:08 +0800

    Set LJPhoto protocol

---
+++ Demo/LJPhotoBrowser/LJPhotoBrowser/Classes/LJPhoto.m
@@ -0,0 +1,18 @@
+//
+//  LJPhoto.m
+//  LJPhotoBrowser
+//
+//  Created by liangju on 5/31/17.
+//  Copyright © 2017 https://liuliangju.github.io. All rights reserved.
+//
+
+#import "LJPhoto.h"
+
+
+@interface LJPhoto ()
+
+@end
+
+@implementation LJPhoto
+
+@end
